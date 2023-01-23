87397194e2129b80b01ae08d52eb7d6d07dec71d
Shakhnazar45 <imramziyev02@gmail.com>
2023-01-23 19:24:47 +0600

    hello

---
+++ main.js
@@ -0,0 +1,13 @@
+// $(function(){
+//     var icons = {
+//         header: "ui-icon-circle-arrow-e",
+//         activeHeader: "ui-icon-circle-arrow-s"
+//       };
+//     $(".crocodile .croc-con #accordion").accordion({
+//         collapsible: true,
+//         icons: icons
+//     });
+// });
+
+
+
